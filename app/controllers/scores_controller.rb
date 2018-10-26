ecf05527dbc2b1956e7709bcda501d39c90ac78c
EthanFe <sneakcow@gmail.com>
2018-10-26 11:18:49 -0500

    leaderboards...?

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,17 @@
 class ScoresController < ApplicationController
   def create
       user = User.find_or_create_by(name: params[:username])
-      user.scores.create(kills: params[:kills], cookies_fired: params[:cookiesFired], cookies_hit: params[:cookiesHit])
+      kills = params[:kills] ? params[:kills] : 0
+      cookies_fired = params[:cookiesFired] ? params[:cookiesFired] : 0
+      cookies_hit = params[:cookiesHit] ? params[:cookiesHit] : 0
+      user.scores.create(kills: kills, cookies_fired: cookies_fired, cookies_hit: cookies_hit)
+  end
+
+  def all
+    render json: (Score.all.map do |score|
+      {name: score.user.name, kills: score.kills, cookies_fired: score.cookies_fired, cookies_hit: score.cookies_hit}
+    end.sort_by do |score|
+      score[:kills]
+    end)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :villains
   get "/heroes_and_villains", to: "heros#get_hero_and_villain_data"
   post "/submit_score", to: "scores#create"
+  get "/scores", to: "scores#all"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
